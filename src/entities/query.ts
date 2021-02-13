52ba990c499b9df900faf7b91c99e33f048f8568
Vincent Raerek <vincent@monadnomad.dev>
2021-02-12 16:20:03 -0800

    Add queryer

---
+++ src/entities/query.ts
@@ -0,0 +1,23 @@
+import { Contract } from '@ethersproject/contracts'
+import MarginTrader from "marginswap-core/build/contracts/MarginTrading.json"
+import { getNetwork } from '@ethersproject/networks'
+import { getDefaultProvider } from '@ethersproject/providers'
+import { ChainId } from '../constants'
+
+export abstract class Query {
+
+    /**
+     * Get holding amounts
+     * @param chainId chain of the token
+     * @param address address of the token on the chain
+     * @param provider provider used to fetch the token
+     */
+    public static async getHoldingAmounts(
+        chainId: ChainId,
+        address: string,
+        provider = getDefaultProvider(getNetwork(chainId))
+    ): Promise<[string[], number[]]> {
+        const marginTrader = new Contract(address, MarginTrader.abi, provider);
+        return marginTrader.functions.getHoldingAmounts(address)
+    }
+}
