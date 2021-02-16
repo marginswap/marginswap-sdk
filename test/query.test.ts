a0da2135ccfba3b3541f55884b8bf3498c762a4e
Vincent Raerek <vincent@monadnomad.dev>
2021-02-16 15:32:37 -0800

    Update query tests

--- test/query.test.ts
+++ test/query.test.ts
@@ -1,6 +1,6 @@
 import { ChainId, Token, Query } from '../src'
 
-describe('Pair', () => {
+describe('Query', () => {
     const USDC = new Token(ChainId.MAINNET, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 18, 'USDC', 'USD Coin')
 
     describe('#getHoldingAmounts', () => {
