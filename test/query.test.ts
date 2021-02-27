43553e4403fdc969e0590c9e37e867a0db23dc9a
Vincent Raerek <vincent@monadnomad.dev>
2021-02-26 16:44:53 -0800

    Update test

--- test/query.test.ts
+++ test/query.test.ts
@@ -1,15 +1,17 @@
-import { ChainId, Token, Query } from '../src'
+import { Query } from '../src'
+import { getDefaultProvider } from '@ethersproject/providers'
 
 describe('Query', () => {
     const ADDRESS_ONE = '0x0000000000000000000000000000000000000001'
 
-    const USDC = new Token(ChainId.ROPSTEN, ADDRESS_ONE, 18, 'USDC', 'USD Coin')
+    // const USDC = new Token(ChainId.ROPSTEN, ADDRESS_ONE, 18, 'USDC', 'USD Coin')
 
     describe('#getHoldingAmounts', () => {
-        it('returns the holding amounts', () => {
-            return Query.getHoldingAmounts(ChainId.ROPSTEN, USDC.address).then(data => {
-                expect(data).toEqual('1')
-            })
+        it('returns empty holding amounts for nonexistent account', () => {
+            const provider = getDefaultProvider("http://localhost");
+            return Query.getHoldingAmounts(ADDRESS_ONE, -1, provider).then(data => {
+                expect(data).toEqual([[], []]);
+            });
         })
     })
 })
