8716664c939c385122d4821fae843bd4eaebc36c
Koochr <dm.girenko@gmail.com>
2021-04-05 16:11:06 +0300

    Update BigNumber typing in lending as well

--- src/entities/lending.ts
+++ src/entities/lending.ts
@@ -5,10 +5,7 @@ import { BaseProvider, getDefaultProvider } from '@ethersproject/providers';
 import { ChainId } from '../constants';
 import * as _ from "lodash";
 import addresses from "@marginswap/core-abi/addresses.json";
-
-type token = string;
-type amount = number;
-type balances = Record<token, amount>;
+import {Balances} from "./margin-account";
 
 function getLending(chainId:ChainId, provider:BaseProvider) {
     const networkName = getNetwork(chainId).name;
@@ -26,7 +23,7 @@ export async function getHourlyBondBalances(
     activeTokens:string[],
     chainId = ChainId.MAINNET,
     provider = getDefaultProvider(getNetwork(chainId))
-): Promise<balances> {
+): Promise<Balances> {
     const lending = getLending(chainId, provider);
     const requests = activeTokens.map(token => lending.viewHourlyBondAmount(token, lenderAddress));
     return Promise.all(requests).then(balances => _.zipObject(activeTokens, balances));
