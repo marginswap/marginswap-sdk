c89505c739f940a4cb242edc1dc36d535eeb4332
Koochr <dm.girenko@gmail.com>
2021-04-05 15:39:03 +0300

    Fix typos and mistakes in import/export

--- src/entities/lending.ts
+++ src/entities/lending.ts
@@ -4,7 +4,7 @@ import { getNetwork } from '@ethersproject/networks';
 import { BaseProvider, getDefaultProvider } from '@ethersproject/providers';
 import { ChainId } from '../constants';
 import * as _ from "lodash";
-import * as addresses from "@marginswap/core-abi/addresses.json";
+import addresses from "@marginswap/core-abi/addresses.json";
 
 type token = string;
 type amount = number;
@@ -12,7 +12,7 @@ type balances = Record<token, amount>;
 
 function getLending(chainId:ChainId, provider:BaseProvider) {
     const networkName = getNetwork(chainId).name;
-    return new Contract((addresses as any)[networkName].Lendning, LendingCore.abi, provider);
+    return new Contract((addresses as any)[networkName].Lending, LendingCore.abi, provider);
 }
 
 /**
