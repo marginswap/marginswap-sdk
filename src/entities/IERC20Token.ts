7a2b204f4569482164d24dd000cbf542a17f1d1b
Koochr <dm.girenko@gmail.com>
2021-04-15 21:02:17 +0300

    Add getTokenBalance function

--- src/entities/IERC20Token.ts
+++ src/entities/IERC20Token.ts
@@ -1,5 +1,15 @@
 import { Provider } from '@ethersproject/providers';
 import { Contract } from '@ethersproject/contracts';
 import abi from '../abis/ERC20.json';
+import { BigNumber } from '@ethersproject/bignumber';
 
 export const getIERC20Token = (address: string, provider: Provider): Contract => new Contract(address, abi, provider);
+
+export const getTokenBalance = async (
+  ownerAddress: string,
+  tokenAddress: string,
+  provider: Provider
+): Promise<BigNumber> => {
+  const token = getIERC20Token(tokenAddress, provider);
+  return token.balanceOf(ownerAddress);
+};
