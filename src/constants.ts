7f38e41f64ad4dd616bb90bab9305ae28b5c78d7
Gabriel Pickard <wergomat@gmail.com>
2021-06-07 10:08:35 -0500

    quickswap config

--- src/constants.ts
+++ src/constants.ts
@@ -42,7 +42,8 @@ export enum AMMs {
   SUSHI,
   PANGOLIN,
   BAGUETTE,
-  SUSHI_ALT
+  SUSHI_ALT,
+  QUICKSWAP
 }
 
 export const ammsPerChain = {
@@ -53,7 +54,8 @@ export const ammsPerChain = {
   [ChainId.FUJI]: [AMMs.PANGOLIN, AMMs.BAGUETTE],
   [ChainId.ROPSTEN]: [AMMs.UNI, AMMs.SUSHI],
   [ChainId.RINKEBY]: [AMMs.UNI, AMMs.SUSHI],
-  [ChainId.GÖRLI]: [AMMs.UNI, AMMs.SUSHI]
+  [ChainId.GÖRLI]: [AMMs.UNI, AMMs.SUSHI],
+  [ChainId.MATIC]: [AMMs.QUICKSWAP, AMMs.SUSHI_ALT]
 };
 
 export const ammOrder: Record<AMMs, number> = {
@@ -61,7 +63,8 @@ export const ammOrder: Record<AMMs, number> = {
   [AMMs.SUSHI]: 1,
   [AMMs.PANGOLIN]: 0,
   [AMMs.SUSHI_ALT]: 1,
-  [AMMs.BAGUETTE]: 2
+  [AMMs.BAGUETTE]: 2,
+  [AMMs.QUICKSWAP]: 0
 };
 
 export const factoryAddresses: Record<AMMs, string> = {
@@ -69,7 +72,8 @@ export const factoryAddresses: Record<AMMs, string> = {
   [AMMs.SUSHI]: '0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac',
   [AMMs.PANGOLIN]: '0xefa94DE7a4656D787667C749f7E1223D71E9FD88',
   [AMMs.BAGUETTE]: '0xBB6e8C136ca537874a6808dBFC5DaebEd9a57554',
-  [AMMs.SUSHI_ALT]: '0xc35DADB65012eC5796536bD9864eD8773aBc74C4'
+  [AMMs.SUSHI_ALT]: '0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
+  [AMMs.QUICKSWAP]: '0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32'
 };
 
 export const amms: Record<string, AMMs> = {
@@ -85,7 +89,8 @@ export const initCodeHashes: Record<string, string> = {
   [factoryAddresses[AMMs.SUSHI]]: '0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303',
   [factoryAddresses[AMMs.PANGOLIN]]: '0x40231f6b438bce0797c9ada29b718a87ea0a5cea3fe9a771abdd76bd41a3e545',
   [factoryAddresses[AMMs.BAGUETTE]]: '0x81dbf51ab39dc634785936a3b34def28bf8007e6dfa30d4284c4b8547cb47a51',
-  [factoryAddresses[AMMs.SUSHI_ALT]]: '0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303'
+  [factoryAddresses[AMMs.SUSHI_ALT]]: '0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303',
+  [factoryAddresses[AMMs.QUICKSWAP]]: '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
 };
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000);
