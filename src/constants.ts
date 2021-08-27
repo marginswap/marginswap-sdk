557122acb65539068bb4208cf285a977338d6759
Gabriel Pickard <wergomat@gmail.com>
2021-08-27 12:34:27 -0500

    bump

--- src/constants.ts
+++ src/constants.ts
@@ -45,14 +45,15 @@ export enum AMMs {
   SUSHI_ALT,
   QUICKSWAP,
   PANCAKESWAP,
-  APESWAP
+  APESWAP,
+  TRADERJOE
 }
 
 export const ammsPerChain = {
   [ChainId.MAINNET]: [AMMs.UNI, AMMs.SUSHI],
   [ChainId.KOVAN]: [AMMs.UNI, AMMs.SUSHI],
   [ChainId.LOCAL]: [AMMs.UNI, AMMs.SUSHI],
-  [ChainId.AVALANCHE]: [AMMs.PANGOLIN, AMMs.SUSHI_ALT],
+  [ChainId.AVALANCHE]: [AMMs.PANGOLIN, AMMs.TRADERJOE],
   [ChainId.FUJI]: [AMMs.PANGOLIN, AMMs.BAGUETTE],
   [ChainId.ROPSTEN]: [AMMs.UNI, AMMs.SUSHI],
   [ChainId.RINKEBY]: [AMMs.UNI, AMMs.SUSHI],
@@ -65,11 +66,12 @@ export const ammOrder: Record<AMMs, number> = {
   [AMMs.UNI]: 0,
   [AMMs.SUSHI]: 1,
   [AMMs.PANGOLIN]: 0,
-  [AMMs.SUSHI_ALT]: 1,
+  [AMMs.SUSHI_ALT]: 1, // TODO this doesn't work on avalanche any more, it's #3
   [AMMs.BAGUETTE]: 2,
   [AMMs.QUICKSWAP]: 0,
   [AMMs.PANCAKESWAP]: 0,
-  [AMMs.APESWAP]: 1
+  [AMMs.APESWAP]: 1,
+  [AMMs.TRADERJOE]: 1
 };
 
 export const factoryAddresses: Record<AMMs, string> = {
@@ -80,7 +82,8 @@ export const factoryAddresses: Record<AMMs, string> = {
   [AMMs.SUSHI_ALT]: '0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
   [AMMs.QUICKSWAP]: '0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32',
   [AMMs.PANCAKESWAP]: '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73',
-  [AMMs.APESWAP]: '0x0841BD0B734E4F5853f0dD8d7Ea041c241fb0Da6'
+  [AMMs.APESWAP]: '0x0841BD0B734E4F5853f0dD8d7Ea041c241fb0Da6',
+  [AMMs.TRADERJOE]: '0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10'
 };
 
 export const amms: Record<string, AMMs> = {
@@ -90,7 +93,8 @@ export const amms: Record<string, AMMs> = {
   [factoryAddresses[AMMs.BAGUETTE]]: AMMs.BAGUETTE,
   [factoryAddresses[AMMs.SUSHI_ALT]]: AMMs.SUSHI_ALT,
   [factoryAddresses[AMMs.PANCAKESWAP]]: AMMs.PANCAKESWAP,
-  [factoryAddresses[AMMs.APESWAP]]: AMMs.APESWAP
+  [factoryAddresses[AMMs.APESWAP]]: AMMs.APESWAP,
+  [factoryAddresses[AMMs.TRADERJOE]]: AMMs.TRADERJOE
 };
 
 export const initCodeHashes: Record<string, string> = {
@@ -101,7 +105,8 @@ export const initCodeHashes: Record<string, string> = {
   [factoryAddresses[AMMs.SUSHI_ALT]]: '0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303',
   [factoryAddresses[AMMs.QUICKSWAP]]: '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f',
   [factoryAddresses[AMMs.PANCAKESWAP]]: '0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5',
-  [factoryAddresses[AMMs.APESWAP]]: '0xf4ccce374816856d11f00e4069e7cada164065686fbef53c6167a63ec2fd8c5b'
+  [factoryAddresses[AMMs.APESWAP]]: '0xf4ccce374816856d11f00e4069e7cada164065686fbef53c6167a63ec2fd8c5b',
+  [factoryAddresses[AMMs.TRADERJOE]]: '0x0bbca9af0511ad1a1da383135cf3a8d2ac620e549ef9f6ae3a4c33c2fed0af91'
 };
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000);
